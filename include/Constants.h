3af912f50a9d89ab96fd391788d7d4fa34a5d918
tone-malone <Thomaseflach@gmail.com>
2025-06-08 12:09:00 -0700

    Add files via upload

--- include/Constants.h
+++ include/Constants.h
@@ -11,18 +11,18 @@ constexpr int CARD_HEIGHT = 110;
 constexpr int CARD_SPACING_Y = 30;
 
 // Font settings
-constexpr char FONT_FILE[] = "../fonts/arial.ttf";
+constexpr char FONT_FILE[] = "fonts/arial.ttf";
 constexpr int FONT_SIZE = 24;
 
 // Texture file paths
-constexpr char SPADE_IMG[]    = "../textures/spade.png";
-constexpr char HEART_IMG[]    = "../textures/heart.png";
-constexpr char DIAMOND_IMG[]  = "../textures/diamond.png";
-constexpr char CLUB_IMG[]     = "../textures/club.png";
-constexpr char CARDBACK_IMG[] = "../textures/cardback.png";
-constexpr char JACK_IMG[]     = "../textures/jack.png";
-constexpr char QUEEN_IMG[]    = "../textures/queen.png";
-constexpr char KING_IMG[]     = "../textures/king.png";
+constexpr char SPADE_IMG[]    = "textures/spade.png";
+constexpr char HEART_IMG[]    = "textures/heart.png";
+constexpr char DIAMOND_IMG[]  = "textures/diamond.png";
+constexpr char CLUB_IMG[]     = "textures/club.png";
+constexpr char CARDBACK_IMG[] = "textures/cardback.png";
+constexpr char JACK_IMG[]     = "textures/jack.png";
+constexpr char QUEEN_IMG[]    = "textures/queen.png";
+constexpr char KING_IMG[]     = "textures/king.png";
 
 // Sound file paths
-constexpr char MOVE_SOUND_FILE[] = "../sounds/move.wav";
+constexpr char MOVE_SOUND_FILE[] = "sounds/move.wav";
